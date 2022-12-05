ec8000c5868ebbb437377e0c5a88fa44eca70a58
Kevin Throckmorton <Kevin.Throckmorton@indianhills.edu>
2022-12-05 09:36:05 -0600

    added another line of code

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Github Example");
+
+        System.out.println("Another display message");
+    }
+}
